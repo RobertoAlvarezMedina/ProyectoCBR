d9d87460eb3b5436adeb50f26710db5d92a4015e
RobertoAlvarezMedina <robert10alvarz@gmail.com>
2018-11-07 21:44:43 -0600

    Actualizacion 7 noviembre

--- CBROriginal/CBR_Web/Models/User.cs
+++ CBROriginal/CBR_Web/Models/User.cs
@@ -11,6 +11,7 @@ namespace CBR_Web.Models
     public class User   
     {
         public string Nombre { get; set; }
+        public string apellido { get; set; }
         public string Correo { get; set; }
         public string Clave { get; set; }
         public string Lugarestudio { get; set; }
@@ -19,7 +20,7 @@ namespace CBR_Web.Models
 
         ConexionDB connect = new ConexionDB();
 
-        public Boolean IngresarUsuario(string nombre, string correo, string clave, string LugarEstudio,string FechaNacimiento,string ReingresoContraseña)
+        public Boolean IngresarUsuario(string nombre, string apellido, string correo,string clave, string institucion,string fechanacimiento)
         {
 
             ConexionDB conexion = new ConexionDB();
@@ -27,17 +28,17 @@ namespace CBR_Web.Models
             string strInsert = string.Empty;
             try
             {
-                strInsert = "INSERT INTO PSD_USUARIO (NOMBRE, CORREO, CLAVE, LUGAR_ESTUDIO, FECHA_NACIMIENTO, REINGRESO_CONTRASENA) " +
-                            " VALUES (@P_NOMBRE,@P_CORREO,@P_CLAVE,@P_LUGAR_ESTUDIO,@P_FECHA_NACIMIENTO,@P_REINGRESO_CONTRASENA)";
+                strInsert = "INSERT INTO dbo.cbr_Usuarios (Nombre, Apellido, Correo,Contrasena,FechaNacimiento,Institucion) " +
+                            " VALUES (@Nombre, @Apellido, @Correo, @Contrasena,@FechaNacimiento, @Institucion)";
 
                 Utils.Utils utils = new Utils.Utils();
                 utils.LimpiarSqlParameterCollection();
-                utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@P_NOMBRE", nombre));
-                utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@P_CORREO", correo));
-                utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@P_CLAVE", clave));
-                utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@P_LUGAR_ESTUDIO", LugarEstudio));
-                utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@P_FECHA_NACIMIENTO", FechaNacimiento));
-                utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@P_REINGRESO_CONTRASENA", ReingresoContraseña));
+                utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@NOMBRE", nombre));
+                utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@Apellido", apellido));
+                utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@Correo", correo));
+                utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@Contrasena", clave));
+                utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@FechaNacimiento", fechanacimiento));
+                utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@Institucion", institucion));
 
 
 
@@ -53,16 +54,17 @@ namespace CBR_Web.Models
             return resultado;
         }
 
-        public int GetPsdUsuarioIdPorCorreo(string correo)
+        public int GetUsuarioPorCorreoYContrasena(string correo,string contrasena)
         {
             ConexionDB conexion = new ConexionDB();
             int resultado = 0;
             try
             {
-                string strSelect = " SELECT PSD_Usuario_ID FROM PSD_Usuario WHERE CORREO = @CORREO ";
+                string strSelect = " SELECT  Correo,Contrasena FROM dbo.cbr_Usuarios WHERE Correo = @CORREO AND Contrasena = @Contrasena ";
                 Utils.Utils utils = new Utils.Utils();
                 utils.LimpiarSqlParameterCollection();
                 utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@CORREO", correo));
+                utils.parameterCollection.Add(new System.Data.SqlClient.SqlParameter("@Contrasena", contrasena));
 
                 resultado = int.Parse(conexion.getDatosBD(strSelect, utils.parameterCollection).Rows[0][0].ToString());
 
