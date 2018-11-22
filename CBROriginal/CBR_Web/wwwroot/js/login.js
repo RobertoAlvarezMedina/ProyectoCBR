1c898ac88ae4fd9d90ef6f1bf68457a4733904ac
Byron Badilla <byronbadilla@outlook.com>
2018-11-22 09:43:05 -0600

    index

--- CBROriginal/CBR_Web/wwwroot/js/login.js
+++
@@ -1,8 +0,0 @@
-﻿var login-submit = document.getElementById("login-submit");
-btn.addEventListener("click", function () {
-    renderHTML();
-};
-function renderHTML() {
-    var htmlString = <p>"Guardado con exito"</p>;
-  
-}
--- CBROriginal/CBR_Web/wwwroot/js/preguntas.js
+++
@@ -1,6 +0,0 @@
-﻿var option=document.getElementById("select");
-
-function renderHTML(option){
-document.getElementById("preguntas").innerHTML =`<center><h1>${option}</h1></center>
-<h2></h2>`
-}
